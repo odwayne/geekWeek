957253349a26a803d86d6bc1b3486603ad8b1612
odwayne <oirving@thoughtworks.com>
2015-10-28 01:52:24 -0400

    functional game, without score tracking

---
+++ public/js/js.js
@@ -0,0 +1,204 @@
+
+var isGameFinished = false,
+isQuestionShown = false,
+hasBuzzerPlayed = false,
+wrongCount = 0;
+qNum = 0,
+displayQuestions = questions.keys(),
+currentQuestion = null,
+currentAnswers = null,
+currentScores = null;
+
+setQuestion = function(num) {
+  if(num >= questions.size) {
+    // end game
+    $('#question p').text('THANK YOU FOR PLAYING!!');
+    isGameFinished = true;
+    return;
+  }
+  currentQuestion = displayQuestions.next().value;
+  currentAnswers = questions.get(currentQuestion);
+  currentScores = scores.get(currentQuestion);
+  $('#question p').text(currentQuestion);
+};
+
+showNextQuestion = function() {
+  setQuestion(qNum);
+  qNum++;
+  rotateHide($('#question img'));
+  rotateShow($('#question p'));
+  isQuestionShown = true;
+};
+
+rotateHide = function(el) {
+  if($(el).attr('data-isShown') === 'false') {return;}
+  $(el).attr('data-isShown', 'false');
+  $(el)
+  .css({'rotateX': '0deg'})
+  .transition({
+    rotateX: '90deg'
+  });
+};
+
+rotateShow = function(el) {
+  if($(el).attr('data-isShown') === 'true') {return;}
+  $(el).attr('data-isShown', 'true');
+  $(el)
+  .css({'rotateX': '90deg'})
+  .transition({
+    rotateX: '0deg'
+  });
+};
+
+resetBoard = function() {
+  rotateShow($('#question img'));
+  rotateHide($('#question p'));
+  resetAnswers();
+  isQuestionShown = false;
+};
+
+resetAnswers = function() {
+  $('.answer-text').each(function(idx, el) {
+    rotateHide(el);
+  });
+  $('.answer-score').each(function(idx, el) {
+    rotateHide(el);
+  });
+};
+
+wrongAnswerAction = function() {
+  if(hasBuzzerPlayed) {return;}
+  $('#x-buzzer')[0].play();
+
+  if(wrongCount == 0) {
+    $("#wrong").dialog({
+      modal: true,
+      open: function(event, ui){
+       setTimeout("$('#wrong').dialog('close')",3000);
+      }
+    });
+    wrongCount++;
+  }
+  else if(wrongCount == 1) {
+    $("#wrong2").dialog({
+      modal: true,
+      open: function(event, ui){
+       setTimeout("$('#wrong2').dialog('close')",3000);
+      }
+    });
+    wrongCount++;
+  }
+  else {
+    $("#wrong3").dialog({
+      modal: true,
+      open: function(event, ui){
+       setTimeout("$('#wrong3').dialog('close')",3000);
+      }
+    });
+    wrongCount = 0;
+  }
+  hasBuzzerPlayed = true;
+};
+
+keyDownHandler = function(evt) {
+  if(evt.keyCode === 39) { // right
+    $('#question').click();
+  }
+  else if(evt.keyCode >= 49 && evt.keyCode <= 54) { // 1 - 6
+    $('#answer' + (evt.keyCode - 48)).click();
+  }
+  else if(evt.keyCode === 37) { // left (back)
+    if(isQuestionShown) {
+      qNum -= 2; if(qNum < 0) {qNum = 0;}
+      resetAnswers();
+      setQuestion(qNum);
+      qNum++;
+    }
+    else {
+      qNum -= 1; if(qNum < 0) {qNum = 0; return;}
+      showNextQuestion();
+    }
+    isGameFinished = false;
+  }
+  else if(evt.keyCode === 88) { // X
+    wrongAnswerAction()
+  }
+};
+
+keyUpHandler = function(evt) {
+  if(evt.keyCode === 88) { // X
+    hasBuzzerPlayed = false;
+  }
+};
+
+checkAnswer = function() {
+  if(currentQuestion === null) {return;}
+  var answer = $('#answerAttempt')[0].value;
+  console.log("Answer: " + answer)
+  var indexOfAnswer = currentAnswers.indexOf(answer);
+  console.log("Index: " + indexOfAnswer)
+  if (indexOfAnswer > -1) {
+    var panel = $(document).find('#answers').find('#answer'+ (indexOfAnswer+1));
+    var answerElement = panel.find('.answer-text')[0];
+    if($(answerElement).attr('data-isShown') === 'true') {return;}
+    var scoreElement = panel.find('.answer-score')[0];
+
+    console.log("Answer Element: " + answerElement)
+    console.log("Score Element: " + scoreElement)
+    console.log("Index after: " + indexOfAnswer)
+
+    var bell = $('#bell-ding')[0];
+    bell.currentTime = 0;
+    bell.play();
+
+    $(answerElement).text(answer);
+    $(scoreElement).text('' + currentScores[indexOfAnswer])
+    rotateShow(answerElement);
+    rotateShow(scoreElement);
+    return;
+  }
+  wrongAnswerAction();
+  setTimeout(function() {
+      hasBuzzerPlayed = false;
+    }, 2000);
+  console.log(answer);
+};
+
+
+$(document).ready(function(){
+
+  $(document).on('keydown', keyDownHandler);
+  $(document).on('keyup', keyUpHandler);
+
+  $('#question').click(function(){
+    if(isGameFinished) {return;}
+    if(isQuestionShown) {
+      resetBoard();
+    } else {
+      $('#blip')[0].play();
+      showNextQuestion();
+    }
+  });
+
+  $('.answer').click(function(){
+    if(!isQuestionShown || isGameFinished) {return;}
+    var answerTxt = $(this).find('.answer-text')[0];
+    if($(answerTxt).attr('data-isShown') === 'true') {return;}
+    var answerVal = $(this).find('.answer-score')[0];
+    var id = $(this).attr('id');
+    var num = parseInt(id.charAt(id.length-1));
+
+    var bell = $('#bell-ding')[0];
+    bell.currentTime = 0;
+    bell.play();
+
+    $(answerTxt).text(currentAnswers[num-1]);
+    var score = scores.get(currentQuestion)[num-1];
+    var valString = '' + score;
+    $(answerVal).text(valString);
+
+    rotateShow(answerTxt);
+    rotateShow(answerVal);
+  });
+
+});
---
+++ public/js/questions-answers.js
@@ -0,0 +1,23 @@
+// questions must be less than 104 characters
+// answers must be less than 18 characters
+var questions = new Map();
+var scores = new Map();
+questions.set("Reason someone might make fun of your car", [
+  'Old',
+  'Dirty',
+  'Color/Paint Job',
+  'Has dents/Beat up',
+  'Too small',
+  'Too big'
+]);
+questions.set("Famous 'Martin'", [
+  'MLK JR',
+  'Martin Lawrence',
+  'Dean Martin',
+  'Martin Sheen',
+  'Steve Martin',
+  'Martin Short'
+]);
+
+scores.set("Reason someone might make fun of your car", [29,23,21,8,5,3]);
+scores.set("Famous 'Martin'", [25,22,19,15,11,8]);
